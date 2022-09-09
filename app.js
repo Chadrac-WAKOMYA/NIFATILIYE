f96dbafd092ffd030882658ffb6c086d7b8069a5
chadrac-WAKOMYA <chadracxx@gmail.com>
2022-09-09 08:27:09 +0200

    heroku test

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 const sequelize = require('./src/db/sequelize')
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 sequelize.initDb()
 
 app.get('/', (req, res) => {
-    res.json({ msg: 'Hello Heroku !'})
+    res.json({ msg: 'Hello Heroku !' })
 })
 
 require('./src/routes/user/findAllUsers')(app)
 
-app.listen(port,()=>console.log(`Notre application Node est demaree sur : http://localhost:${port}`))
+app.listen(port, () => console.log(`Notre application Node est demaree sur : http://localhost:${port}`))
